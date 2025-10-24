afe6b56856d9d519e1ba77228b4f35fc2cb9590d
danielcnoronha <205429835+danielcnoronha@users.noreply.github.com>
2025-10-23 22:56:26 -0300

    metade do projeto

---
+++ Concessionaria/moto.java
@@ -0,0 +1,33 @@
+public class Moto extends Veiculo {
+    
+    private int cilindradas;
+
+
+    public Moto(String placa, String marca, String modelo, int ano, double valor, int cilindradas) {
+        super(placa, marca, modelo, ano, valor); (Veiculo)
+        this.cilindradas = cilindradas;
+    }
+
+    
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    
+    @Override
+    public double calcularIpva() {
+        return getValor() * 0.025;
+    }
+
+    
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes(); 
+        System.out.println("Cilindradas: " + cilindradas + " cc");
+        System.out.println("IPVA (2.5%): R$ " + calcularIpva());
+    }
+}
